3b24b0ae02a6cdf2fe3c1304b2f1d07f3e48ccbb
pancake <pancake@nopcode.org>
2026-09-20 17:28:05 +0200

    Release 1.7.11

--- MaiCore/Sources/mai/MaiCLI.swift
+++ MaiCore/Sources/mai/MaiCLI.swift
@@ -932,7 +932,7 @@ private actor TerminalApprovalHandler: ApprovalHandler {
 
 @main
 struct MaiCLI {
-  private static let version = "1.7.10"
+  private static let version = "1.7.11"
 
   static func main() async {
     let environment = ProcessInfo.processInfo.environment
